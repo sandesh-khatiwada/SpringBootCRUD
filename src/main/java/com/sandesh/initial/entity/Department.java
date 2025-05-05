82c890f71693936b4d2bc626247322dd0810bf87
Sandesh Khatiwada <khatiwadasandesh01@gmail.com>
2025-05-05 14:49:52 +0545

    Add unit tests for department service

--- src/main/java/com/sandesh/initial/entity/Department.java
+++ src/main/java/com/sandesh/initial/entity/Department.java
@@ -2,20 +2,19 @@ package com.sandesh.initial.entity;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 
 public class Department {
 
@@ -25,6 +24,7 @@ public class Department {
 
     @NotBlank(message="Department Name can't be empty")
     @Length(max = 10, min = 3, message = "Department name should be at least 3 and at most 5 characters")
+    @Column(unique = true)
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
---
+++ src/test/java/com/sandesh/initial/service/DepartmentServiceTest.java
@@ -0,0 +1,55 @@
+package com.sandesh.initial.service;
+
+import com.sandesh.initial.entity.Department;
+import com.sandesh.initial.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DepartmentServiceTest {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
+    @BeforeEach
+    void setUp() {
+
+        Department department = Department.builder()
+                                .departmentId(1)
+                                .departmentName("IT")
+                                .departmentAddress("Address")
+                                .departmentCode("IT001")
+                                .build();
+
+        Mockito.when(departmentRepository.findByDepartmentName("IT")).thenReturn(department);
+    }
+
+    @Test
+    @DisplayName("Get department result based on valid department name")
+    public void whenValidDepartmentName_DepartmentShouldReturn(){
+        String departmentName="IT";
+        Department result = departmentService.findDepartmentByName(departmentName);
+        assertEquals(departmentName,result.getDepartmentName());
+    }
+
+    @Test
+    @DisplayName("No result if department name is invalid")
+    public void whenInvalidDepartmentName_DepartmentShouldNotReturn(){
+        String departmentName="Non existent";
+        Department result = departmentService.findDepartmentByName(departmentName);
+        assertEquals("","");
+    }
+
+
+}
